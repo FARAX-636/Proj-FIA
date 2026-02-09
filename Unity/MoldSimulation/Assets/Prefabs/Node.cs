27cdf6fc03870d22a8ff39f0efa4d4fddc2730c8
angela-setola <a.setola2@studenti.unisa.it>
2026-02-09 22:30:41 +0100

    Modifiche in Node e Mold
    Il Node è stato modificato per essere utilizzabile anche dall'algoritmo BFS (ad esempio, se usato il BFS non vengono mostrati i valori f, g e h).
    In Mold, il comportamento dell'espansione muffa cambia a seconda dell'algoritmo scelto.
    l'UIHandler ora permette la scelta dell'algoritmo da visualizzare.

--- Unity/MoldSimulation/Assets/Prefabs/Node.cs
+++ Unity/MoldSimulation/Assets/Prefabs/Node.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
+// node per il BFS
 public class Node : MonoBehaviour
 {
     public bool isObstacle = false;
+    public bool previouslyExploredByBFS = false;
+	public Node parent = null;
 	public float g = float.PositiveInfinity;
 	public float h = 0f;
 	public float f = 0f;
-	public Node parent = null;
+    public List<Node> neighbors = new List<Node>();
     public SpriteRenderer spriteRenderer;
     public Color pathColor;
     public Color obstacleColor;
     public Transform [] points;
     public Vector2 [] directions;
-    public List<Node> neighbors;
     public LayerMask layerMask;
     private int i = 0;
-    [SerializeField]    private Text fValue;
+    [SerializeField]   private Text fValue;
     [SerializeField]    private Text gValue;
     [SerializeField]    private Text hValue;
-    
 
-    // Start is called before the first frame update
     void Start()
     {
         spriteRenderer.color = pathColor;
@@ -54,17 +54,23 @@ public class Node : MonoBehaviour
             RaycastHit2D hit2D = Physics2D.Raycast(point.position, directions[i], 0.75f, layerMask);
             if(hit2D.collider != null)
             {
-                //Debug.Log(gameObject.name + " sees " + hit2D.collider.gameObject.name);
                 neighbors.Add(hit2D.collider.gameObject.GetComponent<Node>());
             }
             i++;
         }
     }
 
+    
     void FixedUpdate()
     {
-        gValue.text = "g: "+ Math.Round(g, 3, MidpointRounding.AwayFromZero);
-        fValue.text = "f: "+ Math.Round(f, 3, MidpointRounding.AwayFromZero);;
-        hValue.text = "h: "+ Math.Round(h, 3, MidpointRounding.AwayFromZero);;
+        // aggiorna i valori di g, h, f ogni frame, in modo da mostrarli in tempo reale durante l'esplorazione
+        if(g!= float.PositiveInfinity)   gValue.text = "g: "+ Math.Round(g, 3, MidpointRounding.AwayFromZero);
+        else    gValue.text = "";
+        
+        if(f!= 0)   fValue.text = "f: "+ Math.Round(f, 3, MidpointRounding.AwayFromZero);
+        else    fValue.text = "";
+        
+        if(h!= 0)   hValue.text = "h: "+ Math.Round(h, 3, MidpointRounding.AwayFromZero);
+        else    hValue.text = "";
     }
 }
--- Unity/MoldSimulation/Assets/Prefabs/UIHandler.cs
+++ Unity/MoldSimulation/Assets/Prefabs/UIHandler.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +10,43 @@ public class UIHandler : MonoBehaviour
     public Camera mainCamera;
     public Scrollbar cameraDimensionsScrollbar;
     public Mold mold;
+    public int useAstar = 0;
+    public Text useAstarButtonText;
+    public FixedJoystick joystick;
 
     void Update()
     {
         mainCamera.orthographicSize = 5f + cameraDimensionsScrollbar.value * 10;
     }
 
+    void FixedUpdate()
+    {
+        UnityEngine.Vector3 cameraMovement = UnityEngine.Vector3.up * joystick.Vertical + UnityEngine.Vector3.right * joystick.Horizontal;
+        mainCamera.transform.position += cameraMovement * 3f * Time.fixedDeltaTime;
+    }
+
+    public void ToggleAStarButton()
+    {
+        if(useAstar == 0)
+        {
+            useAstar = 1;
+            useAstarButtonText.text = "Use BFS";
+        }
+        else if(useAstar == 1)
+        {
+            useAstar = 2;
+            useAstarButtonText.text = "Use BFS + A*";
+        }
+        else
+        {
+            useAstar = 0;
+            useAstarButtonText.text = "Use A*";
+        }
+    }
+
     public void Play()
     {
-        mold.StartSearch();
+        mold.StartSearch(useAstar);
     }
 
     public void Close()
